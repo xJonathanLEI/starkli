d061b38909377ca4f2af1c9bb79fc1d39afd6eb0
Jonathan LEI <me@xjonathan.dev>
2023-06-01 10:31:06 +0000

    feat: cairo 1 class declaration

--- src/subcommands/declare.rs
+++ src/subcommands/declare.rs
@@ -87,11 +87,52 @@ impl Declare {
         //   https://github.com/xJonathanLEI/starknet-rs/issues/392
 
         #[allow(clippy::redundant_pattern_matching)]
-        let (class_hash, declaration_tx_hash) = if let Ok(_) =
+        let (class_hash, declaration_tx_hash) = if let Ok(class) =
             serde_json::from_reader::<_, SierraClass>(std::fs::File::open(&self.file)?)
         {
             // Declaring Cairo 1 class
-            todo!("Cairo 1 clas declaration not implemented");
+            let class_hash = class.class_hash()?;
+
+            eprintln!(
+                "Declaring Cairo 1 class: {}",
+                format!("{:#064x}", class_hash).bright_yellow()
+            );
+            eprintln!("Compiling Sierra class to CASM with compiler version v1.1.0...");
+
+            // Code adapted from the `starknet-sierra-compile` CLI
+
+            // TODO: directly convert type without going through JSON
+            let contract_class: cairo_lang_starknet::contract_class::ContractClass =
+                serde_json::from_str(&serde_json::to_string(&class)?)?;
+
+            // TODO: implement the `validate_compatible_sierra_version` call
+
+            // TODO: allow manually specifying casm hash (for advanced users)
+            // TODO: add `starknet-sierra-compile` version to CLI version display
+            // TODO: allow shelling out local `starknet-sierra-compile` installations
+            let casm_contract =
+                cairo_lang_starknet::casm_contract_class::CasmContractClass::from_contract_class(
+                    contract_class,
+                    false,
+                )?;
+
+            // TODO: directly convert type without going through JSON
+            let casm_class =
+                serde_json::from_str::<CompiledClass>(&serde_json::to_string(&casm_contract)?)?;
+
+            let casm_class_hash = casm_class.class_hash()?;
+
+            eprintln!(
+                "CASM class hash: {}",
+                format!("{:#064x}", casm_class_hash).bright_yellow()
+            );
+
+            // TODO: make buffer configurable
+            let declaration = account
+                .declare(Arc::new(class.flatten()?), casm_class_hash)
+                .fee_estimate_multiplier(1.5f64);
+
+            (class_hash, declaration.send().await?.transaction_hash)
         } else if let Ok(_) =
             serde_json::from_reader::<_, CompiledClass>(std::fs::File::open(&self.file)?)
         {
