b930fd902eb7ed19e89c3f292685e087291860cd
Jonathan LEI <me@xjonathan.dev>
2023-06-13 10:34:51 +0000

    feat: specify keystore file via env var
    Adds support for specifying keystore file via the `STARKNET_KEYSTORE`
    environment variable. What's special about this one compared to other
    env var support is that `STARKNET_KEYSTORE` is weak - the keystore
    option does not conflict with other signer options when provided via
    env var. This allows users to have a global "default key" set via shell
    profile, yet still be able to use other signers ad-hoc without unsetting
    the env var first.

--- src/signer.rs
+++ src/signer.rs
@@ -29,7 +29,7 @@ pub enum AnySignerSignError {
 #[derive(Debug, Clone, Parser)]
 pub struct SignerArgs {
     #[clap(long, help = "Path to keystore JSON file")]
-    keystore: Option<PathBuf>,
+    keystore: Option<String>,
     #[clap(
         long,
         help = "Supply keystore password from command line option instead of prompt"
@@ -37,6 +37,11 @@ pub struct SignerArgs {
     keystore_password: Option<String>,
     #[clap(long, help = "Private key in hex in plain text")]
     private_key: Option<String>,
+}
+
+enum StringValue {
+    FromCommandLine(String),
+    FromEnvVar(String),
 }
 
 #[cfg_attr(not(target_arch = "wasm32"), async_trait)]
@@ -64,47 +69,75 @@ impl Signer for AnySigner {
 
 impl SignerArgs {
     pub fn into_signer(self) -> Result<AnySigner> {
-        match (self.keystore, self.keystore_password, self.private_key) {
-            (Some(keystore), keystore_password, None) => {
-                if keystore_password.is_some() {
-                    eprintln!(
-                        "{}",
-                        "WARNING: setting keystore passwords via --password is generally \
-                        considered insecure, as they will be stored in your shell history or other \
-                        log files."
-                            .bright_magenta()
-                    );
-                }
+        // We're not using the `env` derive from `clap` because we need to distinguish between
+        // whether the value is supplied from the command line or the environment variable.
+        //
+        // This distinction is important because we would not yell at the user for having option
+        // conflicts from env vars. This allows us to reject conflicts on options provided from the
+        // command line while ignoring those from env vars.
+        let keystore = match self.keystore {
+            Some(value) => Some(StringValue::FromCommandLine(value)),
+            None => match std::env::var("STARKNET_KEYSTORE") {
+                Ok(value) => Some(StringValue::FromEnvVar(value)),
+                Err(_) => None,
+            },
+        };
 
-                if !keystore.exists() {
-                    anyhow::bail!("keystore file not found");
-                }
-
-                let password = if let Some(password) = keystore_password {
-                    password
-                } else {
-                    rpassword::prompt_password("Enter keystore password: ")?
-                };
-
-                let key = SigningKey::from_keystore(keystore, &password)?;
-
-                Ok(AnySigner::LocalWallet(LocalWallet::from_signing_key(key)))
+        match (keystore, self.keystore_password, self.private_key) {
+            (Some(StringValue::FromCommandLine(keystore)), keystore_password, None) => {
+                Self::resolve_keystore(keystore, keystore_password)
             }
-            (None, None, Some(private_key)) => {
-                // TODO: change to recommend hardware wallets when they become available
-                eprintln!(
-                    "{}",
-                    "WARNING: using private key in plain text is highly insecure, and you should \
-                    ONLY do this for development. Consider using an encrypted keystore instead."
-                        .bright_magenta()
-                );
-
-                let private_key = FieldElement::from_hex_be(&private_key)?;
-                let key = SigningKey::from_secret_scalar(private_key);
-
-                Ok(AnySigner::LocalWallet(LocalWallet::from_signing_key(key)))
+            (None, None, Some(private_key)) => Self::resolve_private_key(private_key),
+            (Some(StringValue::FromEnvVar(_)), None, Some(private_key)) => {
+                Self::resolve_private_key(private_key)
+            }
+            (Some(StringValue::FromEnvVar(keystore)), keystore_password, None) => {
+                Self::resolve_keystore(keystore, keystore_password)
             }
             _ => Err(anyhow::anyhow!("no valid signer option provided")),
         }
+    }
+
+    fn resolve_keystore(keystore: String, keystore_password: Option<String>) -> Result<AnySigner> {
+        let keystore = PathBuf::from(&keystore);
+
+        if keystore_password.is_some() {
+            eprintln!(
+                "{}",
+                "WARNING: setting keystore passwords via --password is generally \
+                        considered insecure, as they will be stored in your shell history or other \
+                        log files."
+                    .bright_magenta()
+            );
+        }
+
+        if !keystore.exists() {
+            anyhow::bail!("keystore file not found");
+        }
+
+        let password = if let Some(password) = keystore_password {
+            password
+        } else {
+            rpassword::prompt_password("Enter keystore password: ")?
+        };
+
+        let key = SigningKey::from_keystore(keystore, &password)?;
+
+        Ok(AnySigner::LocalWallet(LocalWallet::from_signing_key(key)))
+    }
+
+    fn resolve_private_key(private_key: String) -> Result<AnySigner> {
+        // TODO: change to recommend hardware wallets when they become available
+        eprintln!(
+            "{}",
+            "WARNING: using private key in plain text is highly insecure, and you should \
+                    ONLY do this for development. Consider using an encrypted keystore instead."
+                .bright_magenta()
+        );
+
+        let private_key = FieldElement::from_hex_be(&private_key)?;
+        let key = SigningKey::from_secret_scalar(private_key);
+
+        Ok(AnySigner::LocalWallet(LocalWallet::from_signing_key(key)))
     }
 }
