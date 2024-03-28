ae0d626e8fa479a7f4fcd8fe4661f59dfab50baa
Jonathan LEI <me@xjonathan.dev>
2024-03-28 19:07:43 +0000

    feat: use compiler 2.6.x for mainnet

--- src/casm.rs
+++ src/casm.rs
@@ -72,11 +72,11 @@ impl CasmArgs {
                 match network {
                     Some(network) => {
                         let auto_version = match network {
-                            Network::Mainnet => CompilerVersion::V2_5_4,
                             Network::Goerli
                             | Network::Sepolia
                             | Network::GoerliIntegration
-                            | Network::SepoliaIntegration => CompilerVersion::V2_6_2,
+                            | Network::SepoliaIntegration
+                            | Network::Mainnet => CompilerVersion::V2_6_2,
                         };
 
                         eprintln!(
