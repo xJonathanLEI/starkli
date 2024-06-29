825adeaff72cb5f00636123ba899d5de6ab850c2
Jonathan LEI <me@xjonathan.dev>
2024-06-30 05:51:50 +0800

    feat: raw hash blind signing with Ledger

--- src/subcommands/signer/ledger/mod.rs
+++ src/subcommands/signer/ledger/mod.rs
@@ -4,6 +4,9 @@ use clap::{Parser, Subcommand};
 mod get_public_key;
 use get_public_key::GetPublicKey;
 
+mod sign_hash;
+use sign_hash::SignHash;
+
 mod app_version;
 use app_version::AppVersion;
 
@@ -17,6 +20,8 @@ pub struct Ledger {
 enum Subcommands {
     #[clap(about = "Retrieve public key from a Ledger device")]
     GetPublicKey(GetPublicKey),
+    #[clap(about = "Blind sign a raw hash with a Ledger device")]
+    SignHash(SignHash),
     #[clap(about = "Retrieve the Starknet app version on a Ledger device")]
     AppVersion(AppVersion),
 }
@@ -25,6 +30,7 @@ impl Ledger {
     pub async fn run(self) -> Result<()> {
         match self.command {
             Subcommands::GetPublicKey(cmd) => cmd.run().await,
+            Subcommands::SignHash(cmd) => cmd.run().await,
             Subcommands::AppVersion(cmd) => cmd.run().await,
         }
     }
---
+++ src/subcommands/signer/ledger/sign_hash.rs
@@ -0,0 +1,42 @@
+use anyhow::Result;
+use clap::Parser;
+use colored::Colorize;
+use starknet::{
+    core::types::Felt,
+    signers::{ledger::LedgerStarknetApp, DerivationPath},
+};
+
+#[derive(Debug, Parser)]
+pub struct SignHash {
+    #[clap(
+        long,
+        help = "A HD wallet derivation path with EIP-2645 standard, such as \
+        \"m/2645'/1195502025'/1470455285'/0'/0'/0\""
+    )]
+    path: DerivationPath,
+    #[clap(help = "The raw hash to be signed")]
+    hash: String,
+}
+
+impl SignHash {
+    pub async fn run(self) -> Result<()> {
+        let hash = Felt::from_hex(&self.hash)?;
+
+        eprintln!(
+            "{}",
+            "WARNING: blind signing a raw hash could be dangerous. Make sure you ONLY sign hashes \
+            from trusted sources. If you're sending transactions, use Ledger as a signer instead \
+            of using this command."
+                .bright_magenta()
+        );
+
+        let ledger = LedgerStarknetApp::new().await?;
+
+        eprintln!("Please confirm the signing operation on your Ledger");
+
+        let signature = ledger.sign_hash(self.path, &hash).await?;
+        println!("0x{}", signature);
+
+        Ok(())
+    }
+}
