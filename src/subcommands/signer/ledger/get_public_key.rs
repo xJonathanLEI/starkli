b4eea71e39282775918b828f81826e362963fa9f
Jonathan LEI <me@xjonathan.dev>
2024-06-30 13:25:23 +0800

    feat: display public key on Ledger by default

--- src/subcommands/signer/ledger/get_public_key.rs
+++ src/subcommands/signer/ledger/get_public_key.rs
@@ -8,9 +8,9 @@ use crate::hd_path::DerivationPathParser;
 pub struct GetPublicKey {
     #[clap(
         long,
-        help = "Display the public key on Ledger's screen for extra security"
+        help = "Do not display the public key on Ledger's screen for confirmation"
     )]
-    display: bool,
+    no_display: bool,
     #[clap(
         value_parser = DerivationPathParser,
         help = "An HD wallet derivation path with EIP-2645 standard, such as \
@@ -23,11 +23,11 @@ impl GetPublicKey {
     pub async fn run(self) -> Result<()> {
         let ledger = LedgerStarknetApp::new().await?;
 
-        if self.display {
+        if !self.no_display {
             eprintln!("Please confirm the public key on your Ledger");
         }
 
-        let public_key = ledger.get_public_key(self.path, self.display).await?;
+        let public_key = ledger.get_public_key(self.path, !self.no_display).await?;
         println!("{:#064x}", public_key.scalar());
 
         Ok(())
